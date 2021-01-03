3fa349b09220c7ebf800da41d7c0155b65f38437
Tom <tom@naaa.de>
2021-01-03 07:16:13 +0100

    automatically generate OVA files, preinstall VirtualBox-Guest-Additions into those OVA files, set password to a generated value and store the information in description of VM

--- VirtualBox/make.sh
+++ VirtualBox/make.sh
@@ -44,15 +44,83 @@ VBoxManage modifyvm "$MACHINENAME" --graphicscontroller vboxsvga
 VBoxManage modifyvm "$MACHINENAME" --audioout on
 VBoxManage modifyvm "$MACHINENAME" --clipboard bidirectional
 
+#we plan to change the password to this random value and keep a record in the description
+BBUSER_PASSWORD="$(cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c10)"
+echo "new password is: $BBUSER_PASSWORD"
+VBoxManage modifyvm "$MACHINENAME" --description "Password for user \"bbuser\" is \"$BBUSER_PASSWORD\""
+
 VBoxManage createhd --filename "$BASEFOLDER/VirtualBox/$MACHINENAME.vdi" --size $(("$HDD_MB")) --format VDI
 VBoxManage storagectl "$MACHINENAME" --name "SATA Controller" --add sata --controller "IntelAhci" --hostiocache on
 VBoxManage storageattach "$MACHINENAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  "$BASEFOLDER/VirtualBox/$MACHINENAME.vdi"
 
-VBoxManage storageattach "$MACHINENAME" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium "$ISO"
-#VBoxManage storageattach "$MACHINENAME" --storagectl "SATA Controller" --port 2 --device 0 --type dvddrive --medium "${GUESTISO##*/}"     
+VBoxManage storageattach "$MACHINENAME" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium "$ISO"     
 VBoxManage modifyvm "$MACHINENAME" --boot1 dvd --boot2 disk --boot3 none --boot4 none
 
 ################################################################################
 # Run Virtualbox VM
 ################################################################################
 VBoxManage startvm "$MACHINENAME" --type gui
+
+# wait until the netinstaller ISO is ejected
+# alternatively check for this: "SATA Controller-1-0"="emptydrive"
+while true; do
+  sleep 1
+  VBoxManage showvminfo --machinereadable "$MACHINENAME" | grep "\"SATA Controller-IsEjected\"=\"on\""
+  
+  if [ $? -eq 0 ]; then
+    echo "CD ejected from $MACHINENAME"
+    break
+  fi
+done
+
+#################################################################################
+# Install Guest-Additions
+#################################################################################
+
+# if guest-additions-iso are already installed to this host machine this medium works
+#VBoxManage storageattach "$MACHINENAME" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium additions
+# but we better be sure to have the ISO downloaded ourselves
+VBoxManage storageattach "$MACHINENAME" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium "${GUESTISO##*/}"
+
+#################################################################################
+# Wait till firefox-esr process is active
+# injecting commands into the VM only works if guest additions are working
+#################################################################################
+while true; do
+	VBoxManage guestcontrol "$MACHINENAME" --username "bbuser" --password "%password%" run --exe /bin/bash -- bash -c "pidof firefox-esr > /dev/null" > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		break
+	fi
+	sleep 1
+done
+
+#################################################################################
+# set password
+#################################################################################
+#change password inside VM to a random password
+VBoxManage guestcontrol "$MACHINENAME" --username "bbuser" --password "%password%" run --exe /bin/bash -- bash -c "echo -e \"%password%\n$BBUSER_PASSWORD\n$BBUSER_PASSWORD\" | passwd bbuser"
+
+# shutdown appliance
+VBoxManage guestcontrol "$MACHINENAME" --username "bbuser" --password "$BBUSER_PASSWORD" run --exe /bin/bash -- bash -c "sleep 10 && sudo /sbin/shutdown -h now"
+
+# wait until VM is powered off
+while true; do
+  sleep 1
+  VBoxManage showvminfo --machinereadable "$MACHINENAME" | grep "VMState=\"poweroff\""
+  
+  if [ $? -eq 0 ]; then
+    echo "$MACHINENAME powered down"
+    break
+  fi
+done
+
+#################################################################################
+# export appliance
+#################################################################################
+VBoxManage export "$MACHINENAME" --output "$BASEFOLDER/Releases/$MACHINENAME.ova" --ovf10 \
+  --options manifest,nomacs \
+  --vsys 0 \
+  --vmname "$MACHINENAME" \
+  --product "BrowserBox" \
+  --producturl "https://github.com/Torxgewinde/BrowserBox" \
+  --description "Password for user \"bbuser\" is \"$BBUSER_PASSWORD\""
